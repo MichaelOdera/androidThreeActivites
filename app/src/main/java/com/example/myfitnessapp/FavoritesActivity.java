bc7f36948fcc846414bf2a702257cab9745a92f3
MichaelOdera <michogelira@gmail.com>
2020-03-02 01:33:53 +0300

    added an array adapter to show the list of all the favorite items that have been added to the favorite Exercises by a click of a particular item

--- app/src/main/java/com/example/myfitnessapp/FavoritesActivity.java
+++ app/src/main/java/com/example/myfitnessapp/FavoritesActivity.java
@@ -2,15 +2,37 @@ package com.example.myfitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class FavoritesActivity extends AppCompatActivity {
+    @BindView(R.id.favoriteExerciseTextView)
+    TextView mFavoriteExerciseTextView;
+    @BindView(R.id.listView)
+    ListView mFavoriteListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+        ButterKnife.bind(this);
 
+        Intent intent = getIntent();
+        String favoriteExercise = intent.getStringExtra("exercise");
+        mFavoriteExerciseTextView.setText(favoriteExercise + " has been added to your favorite exercises");
+        ArrayList<String> favoriteExerciseList =  new ArrayList<>();
+        favoriteExerciseList.add(favoriteExercise);
+
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, favoriteExerciseList);
+        mFavoriteListView.setAdapter(adapter);
 
     }
 }
