48e9fe01bb3fd43ef2a05f8bb607095e0fe763b9
MichaelOdera <michogelira@gmail.com>
2020-03-20 17:59:38 +0300

    modified the instrumentation test to allow for smooth running of the application on testing at DashboardActivityInstrumentationTest.java

--- app/src/androidTest/java/com/example/myfitnessapp/DashboardActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/example/myfitnessapp/DashboardActivityInstrumentationTest.java
@@ -1,8 +1,5 @@
 package com.example.myfitnessapp;
 
-import android.view.View;
-
-import androidx.test.espresso.ViewAction;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
@@ -10,21 +7,7 @@ import androidx.test.runner.AndroidJUnit4;
 import com.example.myfitnessapp.ui.DashboardActivity;
 
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static androidx.test.espresso.Espresso.onData;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.RootMatchers.isTouchable;
-import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
-import static androidx.test.espresso.matcher.ViewMatchers.isSelected;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -35,16 +18,6 @@ public class DashboardActivityInstrumentationTest {
     public ActivityTestRule<DashboardActivity> dashBoardActivityTestRule =
             new ActivityTestRule<>(DashboardActivity.class);
 
-//    @Test
-//    public void checksTheFirstItemOfTheAdapterListAndReturnsCorrectValue(){
-//        View activityDecorView = dashBoardActivityTestRule.getActivity().getWindow().getDecorView();
-//        String exerciseName = "Running";
-//        onData(anything())
-//                .inAdapterView(withId(R.id.listView))
-//                .atPosition(0)
-//                .perform((ViewAction) isSelected());
-//        onView(withText(exerciseName)).inRoot(withDecorView(not(activityDecorView)))
-//                .check(matches(withText(exerciseName)));
-//    }
+
 
 }
