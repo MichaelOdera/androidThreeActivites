2291b503ce8c35f2912f9c62748c341b3bb21c94
MichaelOdera <michogelira@gmail.com>
2020-03-22 05:07:55 +0300

    inlcluded a function to fetch searchedLocation from the FirebaseDatbase and added a valueEventListener to listen for data change and long the various locations changed onto the Logcat. This was done in the GymnasiumListActivity.java

--- app/src/main/java/com/example/myfitnessapp/ui/GymnasiumListActivity.java
+++ app/src/main/java/com/example/myfitnessapp/ui/GymnasiumListActivity.java
@@ -1,11 +1,13 @@
 package com.example.myfitnessapp.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,11 +17,19 @@ import android.widget.TextView;
 import com.example.myfitnessapp.adapters.GymListAdapter;
 import com.example.myfitnessapp.R;
 import com.example.myfitnessapp.models.Business;
+import com.example.myfitnessapp.models.Constants;
 import com.example.myfitnessapp.network.YelpBusinessesSearchResponse;
 import com.example.myfitnessapp.network.YelpApi;
 import com.example.myfitnessapp.network.YelpClient;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,6 +38,10 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class GymnasiumListActivity extends AppCompatActivity {
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
+    private DatabaseReference mSearchedLocationReference;
 
     @BindView(R.id.recyclerView)
     RecyclerView mRecyclerView;
@@ -50,6 +64,30 @@ public class GymnasiumListActivity extends AppCompatActivity {
         Intent gymLocationIntent = getIntent();
         String location = gymLocationIntent.getStringExtra("gymLocation");
         //mLocationTextView.setText("Gymnasiums near "+ location);
+
+        mSearchedLocationReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
+
+        mSearchedLocationReference.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("LongLogTag")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d("In the Onchange----------- :", "Got location");
+                for(DataSnapshot locationSnapshot : dataSnapshot.getChildren()){
+                    AtomicReference<String> location = new AtomicReference<>(Objects.requireNonNull(locationSnapshot.getValue(String.class)));
+                    Log.d("Locations updated", "location: " + location);
+                    System.out.println("Location: "+location);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+
 
         YelpApi client = YelpClient.getClient();
         Call<YelpBusinessesSearchResponse> call = client.getGymnasiums(location, "gyms");
