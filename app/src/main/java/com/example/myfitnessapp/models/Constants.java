0c46dc8a015a76ca1b1983b902edccc8b6eb9dd1
MichaelOdera <michogelira@gmail.com>
2020-03-22 04:56:30 +0300

    added preference values for location and also the FIREBASE_CHILD_GYMNASIUMS string named 'gymnasiums' to allow for calling the name without having to call it as gymnaiums every time it needs to be called. This was saved in the Constants.java file

--- app/src/main/java/com/example/myfitnessapp/models/Constants.java
+++ app/src/main/java/com/example/myfitnessapp/models/Constants.java
@@ -10,5 +10,8 @@ public class Constants {
         public static final String YELP_BASE_URL = "https://api.yelp.com/v3/";
         public static final String YELP_API_KEY = BuildConfig.YELP_API_KEY;
         public static final String GOOGLE_API_KEY = BuildConfig.GOOGLE_API_KEY;
-
+        public static final String FIREBASE_CHILD_SEARCHED_LOCATION = "searchedLocation";
+        public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+        public static final String PREFERENCES_LOCATION_KEY = "location";
+        public static final String FIREBASE_CHILD_GYMNASIUMS = "gymnasiums";
 }
