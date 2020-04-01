cab08117d657b6b8c8628c40f957949192fa29b4
MichaelOdera <michogelira@gmail.com>
2020-04-02 02:20:45 +0300

    added header using okHttpClient Builder pattern in YoutubeClient.java instance class

--- app/src/main/java/com/example/myfitnessapp/network/YoutubeClient.java
+++ app/src/main/java/com/example/myfitnessapp/network/YoutubeClient.java
@@ -17,21 +17,21 @@ public class YoutubeClient {
 
     public static YoutubeApi getClient(){
         if (retrofit == null){
-//            OkHttpClient okHttpClient = new OkHttpClient.Builder()
-//                    .addInterceptor(new Interceptor() {
-//                        @Override
-//                        public Response intercept(Chain chain) throws IOException {
-//                            Request newRequest = chain.request().newBuilder()
-//                                    .addHeader("Authorization", GOOGLE_API_KEY)
-//                                    .build();
-//                            return chain.proceed(newRequest);
-//                        }
-//                    })
-//                    .build();
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request newRequest = chain.request().newBuilder()
+                                    .addHeader("key", GOOGLE_API_KEY)
+                                    .build();
+                            return chain.proceed(newRequest);
+                        }
+                    })
+                    .build();
 
             retrofit = new Retrofit.Builder()
                     .baseUrl(YOUTUBE_API_URL)
-                    //.client(okHttpClient)
+                    .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
