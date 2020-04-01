477a940fae6eeee54c9138b6ec0bc9600ee8ddd9
MichaelOdera <michogelira@gmail.com>
2020-04-01 21:08:33 +0300

    added YoutubeApi Interface class that carries the end point together with the queries for the Google Api used to fetch video details from

--- app/src/main/java/com/example/myfitnessapp/network/YoutubeApi.java
+++ app/src/main/java/com/example/myfitnessapp/network/YoutubeApi.java
@@ -11,6 +11,7 @@ public interface YoutubeApi {
     Call<GoogleResponse> getVideos(
             @Query("part") String part,
             @Query("q") String q,
+            @Query("maxResults") String maxResults,
             @Query("key") String key
     );
 }
