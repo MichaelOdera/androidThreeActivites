5a9f7e512d5532123041f5e29217160fbd85febe
MichaelOdera <michogelira@gmail.com>
2020-04-01 20:09:13 +0300

    added YoutubePlayerActivity.java to the AndroidManifest.xml file

---
+++ app/src/main/java/com/example/myfitnessapp/ui/YoutubePlayerActivity.java
@@ -0,0 +1,13 @@
+package com.example.myfitnessapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YoutubePlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube_player);
+    }
+}
