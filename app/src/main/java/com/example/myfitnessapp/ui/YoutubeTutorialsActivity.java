28e00511a76538c07ab8ef46704b9299633dfc60
MichaelOdera <michogelira@gmail.com>
2020-04-01 21:29:41 +0300

    Created a method onCreateOptionsMenu in the YoutubeTutorialsActivity.java to allow for a reuse of the menu_searc.xml file which contains a SearchView to allow for the user to search for videos

--- app/src/main/java/com/example/myfitnessapp/ui/YoutubeTutorialsActivity.java
+++ app/src/main/java/com/example/myfitnessapp/ui/YoutubeTutorialsActivity.java
@@ -1,13 +1,22 @@
 package com.example.myfitnessapp.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.widget.MediaController;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.VideoView;
 
@@ -30,6 +39,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class YoutubeTutorialsActivity extends AppCompatActivity {
+    @BindView(R.id.searchVideosHeader) TextView mSearchVideosHeader;
     @BindView(R.id.youtubeRecyclerView) RecyclerView mYoutubeRecyclerView;
     public List<Item> mItems;
     private YoutubeRecyclerAdapter youtubeRecyclerAdapter;
@@ -41,13 +51,48 @@ public class YoutubeTutorialsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_youtube_tutorials);
         ButterKnife.bind(this);
 
+
+
+    }
+    @SuppressLint("CommitPrefEdits")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        ButterKnife.bind(this);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+        searchView.setSubmitButtonEnabled(true);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                getVideos(query);
+                return false;
+            }
+
+
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return true;
+
+    }
+
+    private void getVideos(String query) {
         YoutubeApi client = YoutubeClient.getClient();
-        Call<GoogleResponse> call = client.getVideos("snippet", "football", GOOGLE_API_KEY);
+        Call<GoogleResponse> call = client.getVideos("snippet", query,"50", GOOGLE_API_KEY);
 
         call.enqueue(new Callback<GoogleResponse>() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onResponse(Call<GoogleResponse> call, Response<GoogleResponse> response) {
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
+                    mSearchVideosHeader.setText("HERE ARE YOUR VIDEOS");
                     mItems = response.body().getItems();
                     RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(YoutubeTutorialsActivity.this);
                     youtubeRecyclerAdapter = new YoutubeRecyclerAdapter(YoutubeTutorialsActivity.this, mItems);
@@ -55,6 +100,7 @@ public class YoutubeTutorialsActivity extends AppCompatActivity {
                     mYoutubeRecyclerView.setAdapter(youtubeRecyclerAdapter);
                     mYoutubeRecyclerView.setHasFixedSize(true);
                     Log.d("Successful------", "SUCCESS________________");
+
 
                 }
 
@@ -69,3 +115,5 @@ public class YoutubeTutorialsActivity extends AppCompatActivity {
 
     }
 }
+
+
