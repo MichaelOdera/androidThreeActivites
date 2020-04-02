d647b1e750317ecb487f8211fa2bb108560cf461
MichaelOdera <michogelira@gmail.com>
2020-04-02 11:53:15 +0300

    added the soom out animation function in the FadeActivity.java class

--- app/src/main/java/com/example/myfitnessapp/ui/FadeActivity.java
+++ app/src/main/java/com/example/myfitnessapp/ui/FadeActivity.java
@@ -2,11 +2,14 @@ package com.example.myfitnessapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,13 +21,17 @@ import butterknife.ButterKnife;
 public class FadeActivity extends AppCompatActivity implements Animation.AnimationListener{
     @BindView(R.id.getStartedButton)
     Button mGetStartedButton;
+    @BindView(R.id.gymnasiumImageView)
+    ImageView mGymnasiumsImageView;
     TextView errorMessage;
 
     // Animation
     Animation animationFadeIn;
+    Animation animationZoomOut;
 
 
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +42,18 @@ public class FadeActivity extends AppCompatActivity implements Animation.Animati
         // load the animation
         animationFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),
                 R.anim.fades_in);
+        animationZoomOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_out);
+
+        mGymnasiumsImageView.setOnTouchListener((v, event) -> {
+            mGymnasiumsImageView.startAnimation(animationZoomOut);
+            return false;
+        });
 
         mGetStartedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 errorMessage.setVisibility(View.VISIBLE);
                 errorMessage.startAnimation(animationFadeIn);
-
             }
         });
     }
